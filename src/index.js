e2c543c0dbfd9ce87138e6b9311a02c73f86e415
Luna <78354242+wxifuwu@users.noreply.github.com>
2022-08-23 17:21:51 +0200

    random dumb fix

--- src/index.js
+++ src/index.js
@@ -12,7 +12,10 @@ export default class EconomyClient {
     constructor (dbURI) {
         if (process.version.slice(1, 3) - 0 < 16) throw new TypeError("NodeJS version 16 or higher is required for this package to work. Go to https://nodejs.org/ to update your NodeJS version");
         if (!dbURI) throw new TypeError("A MongoDB URL was not provided");
+        
+        setTimeout(async () => {
         await mongoose.connect(dbURI);
+        }, 1000)
 
         this.users = new UserManager();
         this.bank = new BankManager();
