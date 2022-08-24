0811851d8b17ea252a3f1feeb172af5bc620ec9f
Cit <94020875+cit999@users.noreply.github.com>
2022-08-25 08:44:13 +1000

    Update shop.js

--- src/models/shop.js
+++ src/models/shop.js
@@ -1,7 +1,7 @@
 import { model, Schema } from "mongoose";
 
 export default model('eco-client-shop', new Schema({
-    id: String,
+    name: String,
     items: { type: Array, default: [] },
     lastUpdated: { type: Date, default: new Date() },
 }));
